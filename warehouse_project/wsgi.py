fffd6b284e087c9e951465ac11d0156e95743b62
Dias Baurzhanuly <107209044+deamarriore@users.noreply.github.com>
2026-05-06 10:14:46 +0500

    Update wsgi.py

--- warehouse_project/wsgi.py
+++ warehouse_project/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for warehouse_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Указываем Django, где лежат настройки
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'warehouse_project.settings')
 
+# Vercel ищет переменную с именем 'app' или 'application'
+# Мы создаем обе, чтобы точно сработало
 application = get_wsgi_application()
+app = application
